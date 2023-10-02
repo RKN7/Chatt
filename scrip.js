4a68367f0aed5d70fbbde2f8660599605404ec70
RKN7 <72651844+RKN7@users.noreply.github.com>
2023-10-02 20:47:23 +0530

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,9 @@
+
+const formPopup= document.querySelector(".form-popup");
+const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
+signupLoginLink.forEach(link => {
+    link.addEventListener("click",(e)=>{
+        e.preventDefault();
+        formPopup.classList[link.id === 'signup-link' ? 'add' : 'remove']("show-signup");
+    });
+});
